1526b84caa6ae3ebaa722dbcdacf969322a6fa9e
Mahesh Dineshkumar(UST <307420@ust.com>
2026-05-12 12:13:37 +0530

    Added load balancer for private vm

--- main.tf
+++ main.tf
@@ -117,94 +117,94 @@ resource "azurerm_linux_virtual_machine" "public_vm" {
 }
 
 resource "azurerm_subnet" "private_subnet" {
-  name                 = var.private_subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
+  name = var.private_subnet_name
+  resource_group_name = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "nat_pip" {
-  name                = var.nat_public_ip_name
-  location            = azurerm_resource_group.rg.location
+  name = var.nat_public_ip_name
+  location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  allocation_method = "Static"
+  sku = "Standard"
 }
 
 resource "azurerm_nat_gateway" "nat" {
-  name                    = var.nat_gateway_name
-  location                = azurerm_resource_group.rg.location
-  resource_group_name     = azurerm_resource_group.rg.name
-  sku_name                = "Standard"
+  name = var.nat_gateway_name
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name = "Standard"
   idle_timeout_in_minutes = 10
 }
 
 resource "azurerm_nat_gateway_public_ip_association" "nat_assoc" {
-  nat_gateway_id       = azurerm_nat_gateway.nat.id
+  nat_gateway_id = azurerm_nat_gateway.nat.id
   public_ip_address_id = azurerm_public_ip.nat_pip.id
 }
 
 resource "azurerm_subnet_nat_gateway_association" "private_nat_assoc" {
-  subnet_id      = azurerm_subnet.private_subnet.id
+  subnet_id = azurerm_subnet.private_subnet.id
   nat_gateway_id = azurerm_nat_gateway.nat.id
 }
 
 resource "azurerm_network_security_group" "private_nsg" {
-  name                = var.private_nsg_name
-  location            = azurerm_resource_group.rg.location
+  name = var.private_nsg_name
+  location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "AllowSSHFromVNet"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "VirtualNetwork"
+    name = "AllowSSHFromVNet"
+    priority = 100
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "22"
+    source_address_prefix = "VirtualNetwork"
     destination_address_prefix = "*"
   }
 
   security_rule {
-    name                       = "AllowHTTPFromVNet"
-    priority                   = 110
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "VirtualNetwork"
+    name = "AllowHTTPFromVNet"
+    priority = 110
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "80"
+    source_address_prefix = "VirtualNetwork"
     destination_address_prefix = "*"
   }
 }
 
 resource "azurerm_subnet_network_security_group_association" "private_subnet_assoc" {
-  subnet_id                 = azurerm_subnet.private_subnet.id
+  subnet_id = azurerm_subnet.private_subnet.id
   network_security_group_id = azurerm_network_security_group.private_nsg.id
 }
 
 resource "azurerm_network_interface" "private_nic" {
-  name                = var.private_nic_name
-  location            = azurerm_resource_group.rg.location
+  name = var.private_nic_name
+  location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "private-ip-config"
-    subnet_id                     = azurerm_subnet.private_subnet.id
+    name = "private-ip-config"
+    subnet_id = azurerm_subnet.private_subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "private_vm" {
-  name                = var.private_vm_name
+  name = var.private_vm_name
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = var.vm_size
-  zone                = "2"
+  location = azurerm_resource_group.rg.location
+  size = var.vm_size
+  zone = "2"
 
-  admin_username                  = var.admin_username
-  admin_password                  = var.admin_password
+  admin_username = var.admin_username
+  admin_password = var.admin_password
   disable_password_authentication = false
 
   network_interface_ids = [
@@ -212,62 +212,62 @@ resource "azurerm_linux_virtual_machine" "private_vm" {
   ]
 
   os_disk {
-    caching              = "ReadWrite"
+    caching = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+    offer = "0001-com-ubuntu-server-jammy"
+    sku = "22_04-lts"
+    version = "latest"
   }
 }
 
 resource "azurerm_public_ip" "lb_pip" {
-  name                = var.lb_public_ip_name
-  location            = azurerm_resource_group.rg.location
+  name = var.lb_public_ip_name
+  location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  allocation_method = "Static"
+  sku = "Standard"
 }
 
 resource "azurerm_lb" "private_lb" {
-  name                = var.lb_name
-  location            = azurerm_resource_group.rg.location
+  name = var.lb_name
+  location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Standard"
+  sku = "Standard"
 
   frontend_ip_configuration {
-    name                 = "lb-frontend"
+    name = "lb-frontend"
     public_ip_address_id = azurerm_public_ip.lb_pip.id
   }
 }
 
 resource "azurerm_lb_backend_address_pool" "backend_pool" {
   loadbalancer_id = azurerm_lb.private_lb.id
-  name            = "backend-pool"
+  name = "backend-pool"
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "private_vm_lb_assoc" {
-  network_interface_id    = azurerm_network_interface.private_nic.id
-  ip_configuration_name   = "private-ip-config"
+  network_interface_id = azurerm_network_interface.private_nic.id
+  ip_configuration_name = "private-ip-config"
   backend_address_pool_id = azurerm_lb_backend_address_pool.backend_pool.id
 }
 
 resource "azurerm_lb_probe" "http_probe" {
   loadbalancer_id = azurerm_lb.private_lb.id
-  name            = "http-probe"
-  port            = 80
-  protocol        = "Tcp"
+  name = "http-probe"
+  port = 80
+  protocol = "Tcp"
 }
 
 resource "azurerm_lb_rule" "http_rule" {
-  loadbalancer_id                = azurerm_lb.private_lb.id
-  name                           = "http-rule"
-  protocol                       = "Tcp"
-  frontend_port                  = 80
-  backend_port                   = 80
+  loadbalancer_id = azurerm_lb.private_lb.id
+  name = "http-rule"
+  protocol = "Tcp"
+  frontend_port = 80
+  backend_port = 80
   frontend_ip_configuration_name = "lb-frontend"
 
   backend_address_pool_ids = [
